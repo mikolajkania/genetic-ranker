79d4f2c32c0040730cef2f7bc5fd5898ad28b43b
Mikolaj Kania <mikolajkania@gmail.com>
2018-03-18 23:57:25 +0100

    Playing with genetic algorithms

--- ranker.py
+++ ranker.py
@@ -1,6 +1,6 @@
 import random
 from deap import creator, base, tools, algorithms
-from math import log1p
+from math import log
 from math import sin
 
 
@@ -15,7 +15,7 @@ class Ranker:
         creator.create("Individual", list, fitness=creator.FitnessMax)
 
         # an attribute ('gene')
-        self.toolbox.register("attr_int", random.randint, -10, 10)
+        self.toolbox.register("attr_int", random.randint, -100, 100)
         # individual consisting of elements ('genes')
         self.toolbox.register("individual", tools.initRepeat, creator.Individual, self.toolbox.attr_int, n=2)
         # list of individuals
@@ -25,42 +25,59 @@ class Ranker:
         # crossover operator
         self.toolbox.register("mate", tools.cxTwoPoint)
         # a mutation operator
-        self.toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
-        # individuals of the current gen are replaced by the 'fittest' of x individuals from the current gen
+        self.toolbox.register("mut_slightly", self.mut_slightly)
+        self.toolbox.register("mutate", self.toolbox.mut_slightly, low=-10, up=10, indpb=0.1)
+        # individuals of the current gen are replaced by the 'fittest'
         self.toolbox.register("select", tools.selTournament, tournsize=2)
 
     # weights must be a tuple so that multi-objective and single objective fitnesses can be treated the same way
     def fitness(self, individual):
         x = individual[0]
         y = individual[1]
-        if y == 0 or x < 0:
-            return -5,
-        return sin(x/y)-log1p(x),
+        if y == 0 or x <= 0:
+            print(str(individual) + ' ' + '-1')
+            return -1,
+        z = log(x) - sin(x / y),
+        # print(str(individual) + ' ' + str(z[0]))
+        return z
 
-    # update individuals's fitness
+        # update individuals's fitness
+
     def evaluate_population_fitnesses(self, pop):
         pop_fit = self.toolbox.map(self.toolbox.evaluate_fun, pop)
         for individual, fitness in zip(pop, pop_fit):
             individual.fitness.values = fitness
+
+    def mut_slightly(self, individual, low, up, indpb):
+        size = len(individual)
+        original = individual[:]
+        for i in range(size):
+            if random.random() < indpb:
+                if individual[i] + random.randint(low, up) > 100:
+                    individual[i] = 100
+                else:
+                    individual[i] += random.randint(low, up)
+        if original != individual:
+            print(str(original) + '(' + str(self.fitness(original)[0]) + ') => ' + str(individual) + '(' + str(self.fitness(individual)[0]) + ')')
+        return individual,
 
 
 def main():
     ranker = Ranker()
 
     # create an initial population of individuals (where each individual is a list of integers)
-    population = ranker.toolbox.population(n=4)
+    population = ranker.toolbox.population(n=10)
 
-    for gen in range(100):
+    for gen in range(20):
         # CXPB  is the probability with which two individuals are crossed
         # MUTPB is the probability for mutating an individual
-        offspring = algorithms.varAnd(population, ranker.toolbox, cxpb=0.5, mutpb=0.1)
+        offspring = algorithms.varAnd(population, ranker.toolbox, cxpb=0.5, mutpb=0.3)
         ranker.evaluate_population_fitnesses(offspring)
         population = ranker.toolbox.select(offspring, k=len(population))
         print(population)
-        top = tools.selBest(population, k=1)
-        print(top)
-        print(ranker.fitness(top[0]))
-        print('\n')
+        print_best(population, ranker)
+
+    print_best(population, ranker)
 
     # fits = [ind.fitness.values[0] for ind in population]
     # length = len(population)
@@ -74,6 +91,10 @@ def main():
     # print("  Std %s" % std)
 
 
+def print_best(population, ranker):
+    top = tools.selBest(population, k=1)
+    print(ranker.fitness(top[0]))
+    print('\n')
 
 
 if __name__ == "__main__":

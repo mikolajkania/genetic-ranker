3db1b5a6c842a0564ccdb0d22c4d9baf497457e2
Mikolaj Kania <mikolajkania@gmail.com>
2018-03-18 20:30:14 +0100

    New evaluation function

--- ranker.py
+++ ranker.py
@@ -1,5 +1,7 @@
 import random
 from deap import creator, base, tools, algorithms
+from math import log1p
+from math import sin
 
 
 class Ranker:
@@ -13,23 +15,27 @@ class Ranker:
         creator.create("Individual", list, fitness=creator.FitnessMax)
 
         # an attribute ('gene')
-        self.toolbox.register("attr_bool", random.randint, 0, 1)
-        # individual consisting of x 'attr_bool' elements ('genes')
-        self.toolbox.register("individual", tools.initRepeat, creator.Individual, self.toolbox.attr_bool, n=5)
+        self.toolbox.register("attr_int", random.randint, -10, 10)
+        # individual consisting of elements ('genes')
+        self.toolbox.register("individual", tools.initRepeat, creator.Individual, self.toolbox.attr_int, n=2)
         # list of individuals
         self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
         # fitness function
         self.toolbox.register("evaluate_fun", self.fitness)
         # crossover operator
         self.toolbox.register("mate", tools.cxTwoPoint)
-        # a mutation operator with a probability to flip each attribute/gene of 0.05
+        # a mutation operator
         self.toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
         # individuals of the current gen are replaced by the 'fittest' of x individuals from the current gen
         self.toolbox.register("select", tools.selTournament, tournsize=2)
 
+    # weights must be a tuple so that multi-objective and single objective fitnesses can be treated the same way
     def fitness(self, individual):
-        # weights must be a tuple so that multi-objective and single objective fitnesses can be treated the same way
-        return sum(individual),
+        x = individual[0]
+        y = individual[1]
+        if y == 0 or x < 0:
+            return -5,
+        return sin(x/y)-log1p(x),
 
     # update individuals's fitness
     def evaluate_population_fitnesses(self, pop):
@@ -44,25 +50,30 @@ def main():
     # create an initial population of individuals (where each individual is a list of integers)
     population = ranker.toolbox.population(n=4)
 
-    for gen in range(20):
+    for gen in range(100):
         # CXPB  is the probability with which two individuals are crossed
         # MUTPB is the probability for mutating an individual
         offspring = algorithms.varAnd(population, ranker.toolbox, cxpb=0.5, mutpb=0.1)
         ranker.evaluate_population_fitnesses(offspring)
         population = ranker.toolbox.select(offspring, k=len(population))
-    top10 = tools.selBest(population, k=10)
-    print(top10)
+        print(population)
+        top = tools.selBest(population, k=1)
+        print(top)
+        print(ranker.fitness(top[0]))
+        print('\n')
 
-    fits = [ind.fitness.values[0] for ind in population]
-    length = len(population)
-    mean = sum(fits) / length
-    sum2 = sum(x * x for x in fits)
-    std = abs(sum2 / length - mean ** 2) ** 0.5
+    # fits = [ind.fitness.values[0] for ind in population]
+    # length = len(population)
+    # mean = sum(fits) / length
+    # sum2 = sum(x * x for x in fits)
+    # std = abs(sum2 / length - mean ** 2) ** 0.5
+    #
+    # print("  Min %s" % min(fits))
+    # print("  Max %s" % max(fits))
+    # print("  Avg %s" % mean)
+    # print("  Std %s" % std)
 
-    print("  Min %s" % min(fits))
-    print("  Max %s" % max(fits))
-    print("  Avg %s" % mean)
-    print("  Std %s" % std)
+
 
 
 if __name__ == "__main__":

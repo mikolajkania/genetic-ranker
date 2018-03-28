ae5c96d672d6240c5913e2d247727c5803736ce4
Mikolaj Kania <mikolajkania@gmail.com>
2018-03-28 23:39:51 +0200

    Queries

--- elastic.py
+++ elastic.py
@@ -2,12 +2,26 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch()
 
-res = es.search(index="articles", body={
-    "query": {
-        "match_all": {}
-    }
-})
 
-print("Got %d Hits:" % res['hits']['total'])
-for hit in res['hits']['hits']:
-    print("[%(score)s] %(title)s" % {'title': hit['_source']['title'], 'score': hit['_score']})
+def search(query):
+    res = es.search(index="articles", body={
+        "query": {
+            "multi_match": {
+                "query": query,
+                "type": "cross_fields",
+                "operator": "OR",
+                "fields": ["title", "tags", "entities", "website", "type", "content_main", "content_additional"]
+            }
+        }
+    })
+    print("For query '%(query)s' got %(hits)d Hits:" % {'query': query, 'hits': res['hits']['total']})
+    for hit in res['hits']['hits']:
+        print("[%(score)s] %(title)s" % {'title': hit['_source']['title'], 'score': hit['_score']})
+    print('\n')
+
+
+search("privacy facebook")
+search("privacy")
+search("trump cambridge")
+search("trump")
+search("guardian")

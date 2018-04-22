d0255982ee9c606d5052e1fc7d2058399f5ae8ce
Mikolaj Kania <mikolajkania@gmail.com>
2018-04-22 16:28:05 +0200

    Verbose option for logging

--- evaluator.py
+++ evaluator.py
@@ -13,20 +13,20 @@ class Evaluator:
             for row in model_reader:
                 self.query_to_id[row[0]] = row[1]
 
-    def quality(self, weights=None):
+    def quality(self, weights=None, verbose=None):
         valid = 0
 
         for key, value in self.query_to_id.items():
             first = self.searcher.search(key, self.fields, weights)
             if first == value:
-                # print('Valid result for query=' + key)
                 valid += 1
-            # else:
-            #     print('Invalid result for query=' + key + ', expected doc with id=' + value)
-            # print()
+            elif verbose:
+                print('Invalid result for query=' + key + ', expected doc with id=' + value)
 
-        quality = valid / len(self.query_to_id)
-        print('Search quality is=' + str(quality))
+        quality = valid / len(self.query_to_id) * 100
+
+        if verbose:
+            print("Search quality is [%(q)d%%] for %(w)s:" % {'q': quality, 'w': weights})
 
         return quality
 
--- ranker.py
+++ ranker.py
@@ -1,7 +1,7 @@
 import random
+
 from deap import creator, base, tools, algorithms
-from math import pow
-from math import copysign
+
 from evaluator import Evaluator
 
 
@@ -27,8 +27,8 @@ class Ranker:
         # crossover operator
         self.toolbox.register("mate", tools.cxTwoPoint)
         # a mutation operator
-        self.toolbox.register("mut_slightly", self.mut_slightly)
-        self.toolbox.register("mutate", self.toolbox.mut_slightly, low=0, up=10)
+        self.toolbox.register("mutate_slightly", self.mutate_random_resetting)
+        self.toolbox.register("mutate", self.toolbox.mutate_slightly, low=0, up=10)
         # individuals of the current gen are replaced by the 'fittest'
         self.toolbox.register("select", tools.selTournament, tournsize=2)
 
@@ -38,26 +38,27 @@ class Ranker:
         # print('For individual=' + str(individual) + ', fitness is=' + str(fitness))
         return fitness,
 
-        # z = pow(individual[0], 2) - pow(individual[1], 2),
-        # # print(str(individual) + ' ' + str(z[0]))
-        # return z
-
     # update individuals's fitness
     def evaluate_population_fitnesses(self, pop):
         pop_fit = self.toolbox.map(self.toolbox.evaluate, pop)
         for individual, fitness in zip(pop, pop_fit):
             individual.fitness.values = fitness
 
-    def mut_slightly(self, individual, low, up):
+    def mutate_random_resetting(self, individual, low, up, verbose=None):
         size = len(individual)
         original = individual[:]
         gene = random.randint(0, size-1)
         individual[gene] = random.randint(low, up)
 
+        if verbose:
+            self.print_different(original, individual)
+
+        return individual,
+
+    def print_different(self, original, individual):
         if original != individual:
             print('muted: ' + str(original) + '(' + str(self.fitness(original)[0]) + ') => ' +
                   str(individual) + '(' + str(self.fitness(individual)[0]) + ')')
-        return individual,
 
 
 def main():
@@ -73,29 +74,16 @@ def main():
     # CXPB  is the probability with which two individuals are crossed
     # MUTPB is the probability for mutating an individual
     population, logbook = algorithms.eaMuPlusLambda(population, ranker.toolbox, mu=MU, lambda_=LAMBDA, cxpb=0.6,
-                                                    mutpb=0.2, ngen=10, halloffame=hof)
+                                                    mutpb=0.2, ngen=20, halloffame=hof)
     # print(population)
     print_best(population, ranker, hof)
-
-    # fits = [ind.fitness.values[0] for ind in population]
-    # length = len(population)
-    # mean = sum(fits) / length
-    # sum2 = sum(x * x for x in fits)
-    # std = abs(sum2 / length - mean ** 2) ** 0.5
-    #
-    # print("  Min %s" % min(fits))
-    # print("  Max %s" % max(fits))
-    # print("  Avg %s" % mean)
-    # print("  Std %s" % std)
-
-    dbg = 1
 
 
 def print_best(population, ranker, hof):
     top = tools.selBest(population, k=1)
     print('\n')
-    print('best of last population: ' + str(top[0]) + ' ' + str(ranker.fitness(top[0])))
-    print('best overall: ' + str(hof[0]) + ' ' + str(ranker.fitness(hof[0])))
+    print('best of last population: {0} {1}%%'.format(str(top[0]), str(ranker.fitness(top[0])[0])))
+    print('best overall: {0} {1}%%'.format(str(hof[0]), str(ranker.fitness(hof[0])[0])))
 
 
 if __name__ == "__main__":
